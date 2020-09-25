6ca0ba7eb9917307e769b649e4ee2394d3a4bd97
davidsancho4 <davidsancho4@gmail.com>
2020-09-25 18:41:37 +0200

    Added login

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -5,14 +5,24 @@ import { User } from '../../entities/user.entity';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from '../user/dtos/register-user.dto';
+import { LoginDto } from './dtos/login.dto';
 
 @ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
+
+    @Post('login')
+    async login(@Body() body: LoginDto) {
+      const user = await this.authService.validateUser(body.email, body.password);
+      if (!user) {
+        throw new UnauthorizedException();
+      }
+      return this.authService.login(user);
+    }
   
-    //@ApiBearerAuth()
-    //@UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    @UseGuards(JwtAuthGuard)
     @ApiOkResponse({ type: User })
     @Post('register')
     async register(@Body() registerDto: RegisterUserDto): Promise<User> {
--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,4 +1,6 @@
 import {forwardRef, Inject, Injectable} from '@nestjs/common';
+import { compare } from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 import { RegisterUserDto } from '../user/dtos/register-user.dto';
 import { UsersService } from '../user/users.service';
 import { User } from '../../entities/user.entity';
@@ -8,8 +10,24 @@ import { User } from '../../entities/user.entity';
 export class AuthService {
   constructor(
     private usersService: UsersService,
+    private jwtService: JwtService,
   ) {}
 
+  async validateUser(email: string, pass: string): Promise<User> {
+    const user = await this.usersService.findByEmail(email);
+    if (user && await compare(pass, user.userPassword)) {
+      return user;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { id: user.id, name: user.name, email: user.email, role: user.role };
+    return {
+      accessToken: this.jwtService.sign(payload),
+      role: user.role
+    };
+  }
 
   async register(userDto: RegisterUserDto): Promise<User> {
     return this.usersService.register(userDto);
