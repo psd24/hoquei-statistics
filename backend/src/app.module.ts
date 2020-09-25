cba046f02a8e2adfe5b4e0d51a72d4b7bbf3009b
Clubapp20 <appclub95@gmail.com>
2020-09-25 06:45:09 +0530

    db connect and authentication

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,17 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { SeedModule } from './seed/seed.module';
 import { Connection } from 'typeorm';
+
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      autoLoadEntities: true
-    })
-  ],
+  imports: [TypeOrmModule.forRoot({
+    autoLoadEntities:true
+  }), AuthModule, UsersModule, SeedModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
-
-  constructor(private connection: Connection) {}
+  constructor(private connection: Connection) {
+   // connection.connect();
+  }
 }
